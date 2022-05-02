6b0d6458101fdbbdca1ad4a4b2423c6f83c7b76e
Nathan Beddoe <nathanb@youi.com>
2022-05-02 12:36:01 +1000

    Remove js preload

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+
+export const config = {
+  unstable_JsPreload: false,
+};
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
