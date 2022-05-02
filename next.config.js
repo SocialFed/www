2170046b2def7c0979d7d3c33166bf7a1a3d94a7
Nathan Beddoe <nathanb@youi.com>
2022-05-02 12:30:29 +1000

    Replace react with preact

--- next.config.js
+++ next.config.js
@@ -2,10 +2,22 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    loader: 'imgix',
-    path: 'https://social-fed.imgix.net',
-    formats: ['image/avif', 'image/webp'],
+    loader: "imgix",
+    path: "https://social-fed.imgix.net",
+    formats: ["image/avif", "image/webp"],
   },
-}
+  webpack: (config, { dev, isServer }) => {
+    // Replace React with Preact only in client production build
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: "preact/compat",
+        "react-dom/test-utils": "preact/test-utils",
+        "react-dom": "preact/compat",
+      });
+    }
 
-module.exports = nextConfig
+    return config;
+  },
+};
+
+module.exports = nextConfig;
